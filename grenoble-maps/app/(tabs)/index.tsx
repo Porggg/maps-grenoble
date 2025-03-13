62648bce273991efd53a6d2973cce065561cfd87
felix <felix.clavel@grenoble-inp.org>
2025-03-13 16:35:32 +0100

    move index

--- grenoble-maps/app/(tabs)/index.tsx
+++ grenoble-maps/app/(tabs)/index.tsx
@@ -1,30 +1,15 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Map from "../components/Map"; // Import du composant Map
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Map />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  },
+  container: { flex: 1 },
 });
